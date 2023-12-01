036ea31fe473546104d7d0f3906304684f82c1d7
Vladyslav Petrov <vladyslav.petrov430@gmail.com>
2023-12-01 21:08:29 +0200

    Create UI project

---
+++ SmartEnergyHub.UI/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartEnergyHub.UI.Models;
+using System.Diagnostics;
+
+namespace SmartEnergyHub.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+    }
+}
