e1ee44e71d84a7740abd5971a7c16914e07e9377
Vladyslav Petrov <vladyslav.petrov430@gmail.com>
2023-12-10 19:36:04 +0200

    Add error page (not found/bad request)

---
+++ SmartEnergyHub.UI/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartEnergyHub.UI.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+
+        public IActionResult BadRequest()
+        {
+            return View();
+        }
+    }
+}
